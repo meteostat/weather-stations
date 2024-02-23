188834b5faf467876ab1a64bc291cf3a5c58a09d
clampr <christian.lamprecht@aol.de>
2024-02-23 07:50:21 +0100

    Add active flags

---
+++ scripts/active.py
@@ -0,0 +1,35 @@
+"""
+Set active flag
+"""
+
+import requests
+import stations
+
+def check_status(url):
+    try:
+        response = requests.head(url)
+        return response.status_code == 404
+    except requests.RequestException:
+        return False
+
+
+def set_active(data: dict, _) -> dict:
+    """
+    Find time zone for weather station
+    """
+
+    if data:
+
+        urls = [
+            f'https://bulk.meteostat.net/v2/hourly/{data["id"]}.csv.gz',
+            f'https://bulk.meteostat.net/v2/daily/{data["id"]}.csv.gz',
+        ]
+
+        not_found = list(map(check_status, urls))
+
+        items = list(data.items())
+        items.insert(1, ('active', False if all(not_found) else True))
+
+        return dict(items)
+
+stations.apply(set_active, threads=1)
